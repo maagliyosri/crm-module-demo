eceb87cf5761bd56dc5605256562dbc867b5b797
Yosri Maagli <yosrimaagli24@gmail.com>
2026-06-24 20:06:51 +0100

    implement client management pages

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { ClientsModule } from './clients/clients.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PrismaModule, ClientsModule],
 })
 export class AppModule {}
